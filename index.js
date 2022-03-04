e0f46cf4b61ac3de03371b06461c261f97995180
BECKY-OCHOLLA <becky.ocholla@student.moringaschool.com>
2022-03-04 22:25:50 +0300

    it will center align the calculator

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 var submit =document.getElementById("submit");
 submit.onclick=()=>{
+    var value=document.getElementById("myName");
     var printDate =document.getElementById ("result");
     var getMyBirthday=document.form.myBirthday.value;
     var myDate=new Date(getMyBirthday);
 
     var myDay=myDate.getDay();
-    var dayArray=["sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday",];
+
+    var dayArray=(["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame",],["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]);
+    
+    if(document.getElementById('male').checked){
+        document.write("male radio button is checked");
+
+    }else if(document.getElementById('female').checked){
+        document.write("female radio button is checked")
+    }else {  
+        document.write("no radio button is selected");   
+}  
+
+ if(male === true){
+     document.write(["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]);
+
+ }else{
+     document.write(["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]);
+ }
+
     printDate.innerHTML=dayArray[myDay];
+
 }
+
+    
