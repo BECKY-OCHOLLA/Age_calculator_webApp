cd1570ddba55f08dd490d5d71b9305df36cf024b
BECKY-OCHOLLA <becky.ocholla@student.moringaschool.com>
2022-03-06 12:04:12 +0300

    it will add js clear button and if statements

--- index.js
+++ index.js
@@ -1,91 +1,85 @@
 
 
-//DOM variables
+    var goBack= document.getElementById("goBack");
+    var date = document.getElementById("month")
+    var month = document.getElementById("date")
+    var year = document.getElementById('year')
+    var birthDay= new Date(year+ '/' +month + '/' + date)
+    var dayOfWeek=birthDay.getDay()
 
-const welcome=document.getElementById("description1");
+    
 
-const myForm=document.getElementById("myForm");
-
-const myOutput=document.getElementById("result");
-
-const goBtn=document.getElementById("goBtn");
-
-const submitBtn= document.getElementById("submit");
-
-const goBack= document.getElementById("goBack");
-
-const akanNAme= document.getElementById("displayAkanName");
-
-goBtn.addEventListener("click", (e)=>{
-    e.preventDefault();
-    description1.style.display="none";
-    myForm.style.display="flex";
-  
-    myForm.classList.add("changeSmooth");
-  })
-
-
-goBack.addEventListener("click", (e)=>{
-    e.preventDefault();
-    description1.style.display="none";
-    myForm.style.display="flex";
-    myOutput.style.display="none";
-  })
-  //Suubmit button event
-  submit.addEventListener("click", (e)=>{
-    e.preventDefault();
-
-
-    let gender= document.form.gender.value;
-
-    let birthDay= document.getElementById("birthDay").value;
-  
-    let birthDayDate= new Date(birthDay);
-
-    let myDay=birthDayDate.getDay();
-
-
-
-
-
-
-
-// var submit =document.getElementById("submit");
-// submit.onclick=()=>{
-   
-//     var printDate =document.getElementById ("result");
-//     var getMyBirthday=document.form.myBirthday.value;
-//     var myDate=new Date(getMyBirthday);
-
-//     var myDay= myDate.getDay();
-
-let maleAkanName= ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]; 
-let femaleAkanName=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]; 
-
-    if(gender===""){
-        alert("Err:Select Your Gender!")
-      }else if(birthDay===""){
-        alert("Err:Enter Your Birthday!")
-      }else{
-        if(gender==="male"){
-          akanNAme.innerHTML= maleAkanName[myDay]
-        }else{
-          akanNAme.innerHTML= femaleAkanName[myDay]
+    function myFunc(year,date,month){
+       
+        if (year.toString().length!=4){
+            alert("incorrect enty,four digits needed i.e 1996");
+            // year.style.border = "solid 3px red";
+            return false;
+            
         }
-        myOutput.style.display="flex";
-        description1.style.display="none";
-        myForm.style.display="none";
+        else if (date.toString().length <2) {
+            alert("incorrect entry ,should 2 digits i.e 06");
+            // date.style.border = "solid 3px red";
+            return false;
+        }
+       
+        else if (month.toString().length <2){
+            alert( "incorrect entry,two digits needed i.e 02");
+            // month.style.border = "solid 3px red";
+            return  false;
+        }
+        else{
+           return true;
+         }
+       
+    }
+  
+    document.getElementById("submitButton").addEventListener("click",function (){
+        var year = document.getElementById('year').value
+        var date = document.getElementById('date').value
+        var month = document.getElementById('month').value
+        
+       
+        var birthDay= new Date(year+ '-' +month + '-' + date)
+       
+        var dayOfWeek=birthDay.getDay()
+       
+    var gender = document.querySelector('input[name="gender"]:checked').value;
     
-        akanNAme.classList.add("type-animation");
     
-        myOutput.classList.add("changeSmooth");
-      }
-    
-     
-    })
 
-// var dayArray=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
-// printDate.innerHTML= dayArray[myDay];
+    var maleName = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    var femaleName = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Amna"];
+    var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    
+    
+     myFunc(year,date,month)
+   
+
+
+    if (Number(date) <=0 && Number(date)>31){
+        document.getElementById("result").innerHTML="Please enter a valid date!"
+    }
+    else if(Number(month)<0 && Number(month)>12){
+        alert("Please enter a valid month!")
+    }
+    else if((gender != "female") && (gender != "male")){
+        alert("Please select gender!")
+    }
+    else if (gender == "male"){
+        document.getElementById("result").innerHTML="Your birth day is "+ days[dayOfWeek]+" while "+" Your Akan name is "+maleName[dayOfWeek]
+    }
+    else if (gender == "female"){
+        document.getElementById("result").innerHTML="Your birth day is "+days[dayOfWeek]+" while "+" Your Akan name is "+femaleName[dayOfWeek]
+    }
+    else{document.getElementById("result").innerHTML=" Please check your entry! "}
+   
+ } )
 
 
     
+
+
+
+
+
